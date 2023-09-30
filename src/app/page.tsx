7eb413ac38e1b8f5608fd8359151319baba50b36
0917ba <0917ba@naver.com>
2023-10-01 03:42:56 +0900

    돌 누르면 발사되는 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 'use client';
 
-import { Bodies, Engine, Render, Runner, World } from 'matter-js';
+import {
+  Bodies,
+  Engine,
+  Render,
+  Runner,
+  World,
+  Mouse,
+  MouseConstraint,
+  Body,
+  Events,
+} from 'matter-js';
 import { useEffect, useRef } from 'react';
 
 export default function Page() {
@@ -18,6 +28,13 @@ export default function Page() {
         wireframes: false,
       },
     });
+
+    // 캔버스 요소에 마우스 이벤트를 추가합니다.
+    const mouse = Mouse.create(render.canvas);
+    const mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+    });
+    World.add(engine.world, mouseConstraint);
 
     const floor = Bodies.rectangle(0, 600, 2160, 40, {
       isStatic: true, // 다른 사물이 통과하지 못함
@@ -40,6 +57,34 @@ export default function Page() {
         },
       },
     });
+
+    function shootStone() {
+      const speed = 15;
+      // 360분법으로 각도를 설정합니다.
+      const angle = 45;
+      // 라디안 및 각도 변환
+      const convertedAngle = (360 - angle) * (Math.PI / 180);
+
+      const x = speed * Math.cos(convertedAngle);
+      const y = speed * Math.sin(convertedAngle);
+
+      const clone = Bodies.circle(190, 465, 20, {
+        label: 'clone',
+        render: {
+          sprite: {
+            texture: '/images/stone.png',
+            xScale: 1,
+            yScale: 1,
+          },
+        },
+      });
+
+      Body.setVelocity(clone, { x, y });
+      World.add(engine.world, clone);
+      World.remove(engine.world, stone);
+    }
+
+    Events.on(mouseConstraint, 'mousedown', shootStone);
 
     World.add(engine.world, [floor, stone]);
     const runner = Runner.run(engine);

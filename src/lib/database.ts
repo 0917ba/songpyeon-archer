5f853636a2472c6d7e316af43a75859825f62255
0917ba <0917ba@naver.com>
2023-10-03 23:36:33 +0900

    error fix 3

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -2,6 +2,6 @@ import { MongoClient } from 'mongodb';
 const mongoPassword = process.env.MONGO_PASSWORD;
 const url = `mongodb+srv://09170917aa213:${mongoPassword}@cluster0.gemfinv.mongodb.net/?retryWrites=true&w=majority`;
 const options = { useNewUrlParser: true } as any;
-let connectDB = new MongoClient(url, options).connect();
+let connectDB = new MongoClient(url, options).connect() as any;
 
 export default connectDB;
